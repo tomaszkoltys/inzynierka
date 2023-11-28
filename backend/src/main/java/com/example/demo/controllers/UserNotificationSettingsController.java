26f133b517732f68f3fb997a49ccfabef759e91f
olavv09 <w.ola0904@gmail.com>
2023-11-28 21:57:49 +0100

    UserNotificationSettingsController fix

--- backend/src/main/java/com/example/demo/controllers/UserNotificationSettingsController.java
+++ backend/src/main/java/com/example/demo/controllers/UserNotificationSettingsController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/review")
+@RequestMapping("/api/v1/notificationsettings")
 public class UserNotificationSettingsController {
 
     @Autowired
